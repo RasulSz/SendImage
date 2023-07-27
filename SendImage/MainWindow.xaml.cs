ab4aa14510658c315e742da4a1d94291a5fba497
XSbzi_uh51 <XSbzi_uh51@itstep.edu.az>
2023-07-27 22:52:38 +0400

    Add project files.

---
+++ SendImage/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SendImage
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            var ipAddress = IPAddress.Parse("192.168.31.90");
+            var port = 80;
+
+            var ep = new IPEndPoint(ipAddress, port);
+
+            try
+            {
+                socket.Connect(ep);
+                if(socket.Connected)
+                {
+                    information.Content = "Connected to server . . .";
+                }
+            }
+            catch (Exception ex)
+            {
+                information.Content = "Cannot Connect to server . . .";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SendBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
